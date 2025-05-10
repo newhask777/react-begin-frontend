a50b7eaa402c91bfc8b013569eb4b57a1a9e6304
newhask777 <newhaskellisp@gmail.com>
2025-05-10 10:47:17 +0300

    exercises and

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-const API_URL = 'http://localhost:5000/api'
+import { TOKEN } from './app.constants'
+
+const API_URL = `${import.meta.env.VITE_SERVER_URL}/api`
 
 export const $axios = axios.create({
 	baseURL: API_URL,
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: Cookies.get('red') ? `Bearer ${Cookies.get('red')}` : ''
+		Authorization: Cookies.get(TOKEN) ? `Bearer ${Cookies.get(TOKEN)}` : ''
 	}
 })
--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -1,19 +1,19 @@
 import Auth from '../components/screens/auth/Auth'
-// import AuthBasic from '../components/screens/auth/AuthBasic'
 import Home from '../components/screens/home/Home'
+import NewExercise from '../components/screens/new-exercise/NewExercise'
 import NewWorkout from '../components/screens/new-workout/NewWorkout'
 import Profile from '../components/screens/profile/Profile'
 
 export const routes = [
 	{
-		path: '/',
-		component: Home,
-		isAuth: false
-	},
-	{
 		path: '/auth',
 		component: Auth,
 		isAuth: false
+	},
+	{
+		path: '/',
+		component: Home,
+		isAuth: true
 	},
 	{
 		path: '/new-workout',
@@ -23,15 +23,14 @@ export const routes = [
 	{
 		path: '/profile',
 		component: Profile,
-		isAuth: false
-	} /* 
+		isAuth: true
+	},
 	{
 		path: '/new-exercise',
-
 		component: NewExercise,
-		isAuth: true,
-	},
-
+		isAuth: true
+	}
+	/*
 	{
 		path: '/workout/:id',
 
